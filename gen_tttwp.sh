d2de7bf686e1192ef5999976db1f86deaa18a278
Gauthier Durieux <gauthier.durieux@cern.ch>
2023-10-28 18:35:03 +0200

    typo in the tttwp patch [thanks to Meng-Ju Tsai]

--- gen_tttwp.sh
+++ gen_tttwp.sh
@@ -38,7 +38,7 @@ PATCH="--- ${OUTDIR}/SubProcesses/P0_gbx_ttxtxwp/matrix_5.f    2023-02-21 18:06:
  C     Amplitude(s) for diagram number 5
        CALL FFV1_0(W(1,14),W(1,12),W(1,9),GC_11,AMP(5))
 -      CALL FFV2_1(W(1,7),W(1,6),GC_124,DCMPLX(CMASS_MDL_MT),W(1,16))
-+      CALL FFV2_2(W(1,7),W(1,6),1.0D-05*GC_124,DCMPLX(173.3,-0.6836),
++      CALL FFV2_1(W(1,7),W(1,6),1.0D-05*GC_124,DCMPLX(173.3,-0.6836),
 +     $   W(1,16))
  C     Amplitude(s) for diagram number 6
        CALL FFV1_0(W(1,5),W(1,16),W(1,15),GC_11,AMP(6))
@@ -50,7 +50,7 @@ PATCH="--- ${OUTDIR}/SubProcesses/P0_gbx_ttxtxwp/matrix_5.f    2023-02-21 18:06:
  C     Amplitude(s) for diagram number 5
        CALL FFV1_0(W(1,14),W(1,12),W(1,9),GC_11,AMP(5))
 -      CALL FFV2_1(W(1,7),W(1,6),GC_124,DCMPLX(CMASS_MDL_MT),W(1,16))
-+      CALL FFV2_2(W(1,7),W(1,6),1.0D-05*GC_124,DCMPLX(173.3,-0.6836),
++      CALL FFV2_1(W(1,7),W(1,6),1.0D-05*GC_124,DCMPLX(173.3,-0.6836),
 +     $   W(1,16))
  C     Amplitude(s) for diagram number 6
        CALL FFV1_0(W(1,5),W(1,16),W(1,15),GC_11,AMP(6))
@@ -62,7 +62,7 @@ PATCH="--- ${OUTDIR}/SubProcesses/P0_gbx_ttxtxwp/matrix_5.f    2023-02-21 18:06:
  C     Amplitude(s) for diagram number 5
        CALL FFV1_0(W(1,14),W(1,12),W(1,9),GC_11,AMP(5))
 -      CALL FFV2_1(W(1,7),W(1,6),GC_124,DCMPLX(CMASS_MDL_MT),W(1,16))
-+      CALL FFV2_2(W(1,7),W(1,6),1.0D-05*GC_124,DCMPLX(173.3,-0.6836),
++      CALL FFV2_1(W(1,7),W(1,6),1.0D-05*GC_124,DCMPLX(173.3,-0.6836),
 +     $   W(1,16))
  C     Amplitude(s) for diagram number 6
        CALL FFV1_0(W(1,5),W(1,16),W(1,15),GC_11,AMP(6))
@@ -74,7 +74,7 @@ PATCH="--- ${OUTDIR}/SubProcesses/P0_gbx_ttxtxwp/matrix_5.f    2023-02-21 18:06:
  C     Amplitude(s) for diagram number 5
        CALL FFV1_0(W(1,14),W(1,12),W(1,9),GC_11,AMP(5))
 -      CALL FFV2_1(W(1,7),W(1,6),GC_124,DCMPLX(CMASS_MDL_MT),W(1,16))
-+      CALL FFV2_2(W(1,7),W(1,6),1.0D-05*GC_124,DCMPLX(173.3,-0.6836),
++      CALL FFV2_1(W(1,7),W(1,6),1.0D-05*GC_124,DCMPLX(173.3,-0.6836),
 +     $   W(1,16))
  C     Amplitude(s) for diagram number 6
        CALL FFV1_0(W(1,5),W(1,16),W(1,15),GC_11,AMP(6))
